531df1277093d492fb4c9a0c7640038ed628097c
Patryk Słomian <malymedalik4@gmail.com>
2023-10-25 10:01:21 +0200

    Lista1, Zadania: 1,2,3,4

---
+++ Programowanie 3 PatSlo/L1Z2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Programowanie_3_PatSlo
+{
+    /// <summary>
+    /// Logika interakcji dla klasy L1Z2.xaml
+    /// </summary>
+    public partial class L1Z2 : Window
+    {
+        public L1Z2()
+        {
+            InitializeComponent();
+        }
+    }
+}
