4067fc4b91fcea66128b293d89f67e6b2364f1f4
Patryk Słomian <malymedalik4@gmail.com>
2023-10-25 09:40:48 +0200

    Dodaj pliki projektów.

---
+++ Programowanie 3 PatSlo/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Programowanie_3_PatSlo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
