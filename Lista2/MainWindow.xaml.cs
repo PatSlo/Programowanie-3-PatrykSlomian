62a4f4c0bb746ea222554c74ba4a81fdcadf5ad9
Patryk Słomian <malymedalik4@gmail.com>
2023-11-08 09:25:54 +0100

    Lista 2: Zadanie1:Kalkulator, Zadanie2: Kółko i krzyżyk

---
+++ Lista2/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lista2
+{
+    public partial class MainWindow : Window
+    {
+        private bool _playerX = true;
+        private Button[,] _buttons;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _buttons = new Button[3, 3] {{Button0, Button1, Button2},
+                                     {Button3, Button4, Button5},
+                                     {Button6, Button7, Button8}};
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Content != null) return;
+
+            button.Content = _playerX ? "X" : "O";
+            _playerX = !_playerX;
+
+            if (CheckForWinner())
+            {
+                MessageBox.Show((_playerX ? "O" : "X") + " wygrywa!");
+                ResetBoard();
+            }
+        }
+
+        private bool CheckForWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (_buttons[i, 0].Content != null && _buttons[i, 0].Content == _buttons[i, 1].Content && _buttons[i, 1].Content == _buttons[i, 2].Content)
+                    return true;
+
+                if (_buttons[0, i].Content != null && _buttons[0, i].Content == _buttons[1, i].Content && _buttons[1, i].Content == _buttons[2, i].Content)
+                    return true;
+            }
+
+            if (_buttons[0, 0].Content != null && _buttons[0, 0].Content == _buttons[1, 1].Content && _buttons[1, 1].Content == _buttons[2, 2].Content)
+                return true;
+
+            if (_buttons[0, 2].Content != null && _buttons[0, 2].Content == _buttons[1, 1].Content && _buttons[1, 1].Content == _buttons[2, 0].Content)
+                return true;
+
+            return false;
+        }
+
+
+        private void ResetBoard()
+        {
+            foreach (Button button in _buttons)
+            {
+                button.Content = null;
+            }
+        }
+    }
+}
---
+++ Lista2_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lista2_Calculator
+{
+    public partial class MainWindow : Window
+    {
+        double tempt = 0;
+        string operation = "Plus";
+        string output = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ((Button)sender).Name;
+            switch (name)
+            {
+                case "Button9":
+                    output += "0";
+                    Result.Text = output;
+                    break;
+
+                case "Button0":
+                    output += "1";
+                    Result.Text = output;
+                    break;
+
+                case "Button1":
+                    output += "2";
+                    Result.Text = output;
+                    break;
+
+                case "Button2":
+                    output += "3";
+                    Result.Text = output;
+                    break;
+
+                case "Button3":
+                    output += "4";
+                    Result.Text = output;
+                    break;
+
+                case "Button4":
+                    output += "5";
+                    Result.Text = output;
+                    break;
+
+                case "Button5":
+                    output += "6";
+                    Result.Text = output;
+                    break;
+
+                case "Button6":
+                    output += "7";
+                    Result.Text = output;
+                    break;
+
+                case "Button7":
+                    output += "8";
+                    Result.Text = output;
+                    break;
+
+                case "Button8":
+                    output += "9";
+                    Result.Text = output;
+                    break;
+            }
+        }
+
+        private void Button_Arithmetic_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ((Button)sender).Name;
+
+            if (name == "Button12")
+            {
+                if (output != "")
+                {
+                    tempt = double.Parse(output);
+                    output = "";
+                    operation = "Plus";
+                }
+            }
+
+            else if (name == "Button13")
+            {
+                if (output != "")
+                {
+                    tempt = double.Parse(output);
+                    output = "";
+                    operation = "Minus";
+                }
+            }
+
+            else if (name == "Button14")
+            {
+                if (output != "")
+                {
+                    tempt = double.Parse(output);
+                    output = "";
+                    operation = "Mult";
+                }
+            }
+
+            else if (name == "Button15")
+            {
+                if (output != "")
+                {
+                    tempt = double.Parse(output);
+                    output = "";
+                    operation = "Div";
+                }
+            }
+        }
+
+        private void Button_Equal_Click(object sender, RoutedEventArgs e)
+        {
+            switch (operation)
+            {
+                case "Plus":
+                    double outputTemp_P = tempt + double.Parse(output);
+                    output = outputTemp_P.ToString();
+                    Result.Text = output;
+                    break;
+
+                case "Minus":
+                    double outputTemp_M = tempt - double.Parse(output);
+                    output = outputTemp_M.ToString();
+                    Result.Text = output;
+                    break;
+
+                case "Mult":
+                    double outputTemp_Ml = tempt * double.Parse(output);
+                    output = outputTemp_Ml.ToString();
+                    Result.Text = output;
+                    break;
+
+                case "Div":
+                    double outputTemp_D = tempt / double.Parse(output);
+                    output = outputTemp_D.ToString();
+                    Result.Text = output;
+                    break;
+            }
+        }
+
+        private void Button_Common_Click(object sender, RoutedEventArgs e)
+        {
+            output = output + ",";
+            Result.Text = output;
+        }
+
+        private void Button_Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(output))
+            {
+                output = output.Substring(0, output.Length - 1);
+                Result.Text = output;
+            }
+        }
+    }
+}
